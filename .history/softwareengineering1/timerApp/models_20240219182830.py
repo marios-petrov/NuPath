f6ad0b3877432f4c2f80b449a9ee7cc91b97d6ba
NateClarkTech <NateClarkTech@gmail.com>
2024-02-19 18:40:30 -0500

    converted other datastructures to models in their own apps.  Only models to make and app for is the points system and notes.

--- .history/softwareengineering1/timerApp/models_20240219182830.py
+++ .history/softwareengineering1/timerApp/models_20240219182830.py
@@ -27,3 +27,19 @@ class TimeSpent(models.Model):
             return timezone.now() - self.startTime
         else:
             raise ValueError("ERROR: timeSpent.py getTimeSpent not possible time error")
+        
+class TimeTracker(models.Model):
+    categoryName = models.CharField(max_length=64)
+    timer = models.ForeignKey(TimeSpent, on_delete=models.CASCADE)
+    
+    def calculateTime(self):
+        return self.timer.getTimeSpent()
+        
+    def startTimer(self):
+        return self.timer.startTimer()
+        
+    def endTimer(self):
+        return self.timer.endTimer()
+
+    def getTimeSinceTimerStart(self):
+        return self.timer.getTimeSinceTimerStart()
--- .history/softwareengineering1/todoListApp/models_20240219183028.py
+++ .history/softwareengineering1/todoListApp/models_20240219183028.py
@@ -1,14 +1,10 @@
-
 from django.db import models
 
-class TodoList(models.Model):
-    name = models.CharField(max_length=64)
-
+# Create your models here.
 class TodoItem(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
-    todo_list = models.ForeignKey(TodoList, related_name='items', on_delete=models.CASCADE)
 
     def is_complete(self):
         return self.completed
@@ -18,5 +14,11 @@ class TodoItem(models.Model):
         self.save()
 
     def mark_not_complete(self):
+        self.completed = False
+        self.save()
+        
+class TodoList(models.Model):
+    name = models.CharField(max_length=64)
+
         self.completed = False
         self.save()
