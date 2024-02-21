58ae1699324ea55be192420537f4614d6cf78e3c
NateClarkTech <NateClarkTech@gmail.com>
2024-02-21 08:59:12 -0500

    fixed malformed file

---
+++ .history/softwareengineering1/notesapp/migrations/0001_initial_20240221085614.py
@@ -0,0 +1,37 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Notes',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('noteText', models.TextField()),
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Notes",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("noteText", models.TextField()),
+            ],
+        ),
+    ]
