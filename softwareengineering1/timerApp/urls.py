22c52014f98dabf95a331a8701f127353468ff7d
laineyyrose <lmrose5522@gmail.com>
2024-02-21 01:41:16 -0500

    fixing timer

--- softwareengineering1/timerApp/urls.py
+++ softwareengineering1/timerApp/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.timer_page, name='timer_page'),
-    
+    path('start_timer/<int:timer_id>/', views.start_timer, name='start_timer'),
+    path('stop_timer/<int:timer_id>/', views.stop_timer, name='stop_timer')
 ]
