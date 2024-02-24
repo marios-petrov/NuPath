ffaf3366e454259eb01d18bc1115827238f4105b
laineyyrose <lmrose5522@gmail.com>
2024-02-23 22:06:11 -0500

    finalizing

--- softwareengineering1/linksapp/models.py
+++ softwareengineering1/linksapp/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Links(models.Model):
+    """
+    Represents a link object with a title, URL, and an optional description.
+
+    Attributes:
+        title (models.CharField): The title of the link.
+        url (models.URLField): The URL the link points to.
+        description (models.TextField): An optional description for the link; can be blank.
+    """
+
     title = models.CharField(max_length=255)
     url = models.URLField()
     description = models.TextField(blank=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the Link object, which is its title.
+
+        Returns:
+            str: The title of the link.
+        """
         return self.title
--- softwareengineering1/welcomeapp/models.py
+++ softwareengineering1/welcomeapp/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Welcome(models.Model):
+    """
+    Represents a welcome message, allowing for dynamic updates to the message text.
+    """
     messageText = models.TextField()
 
     def updateMessageText(self, newMessageText):
+        """
+        Updates the welcome message text with a new message.
+
+        Args:
+            newMessageText (str): The new message text to replace the current message.
+        """
         self.messageText = newMessageText
         self.save()
 
     def clearMessageText(self):
+        """
+        Clears the current message text, setting it to an empty string.
+        """
         self.messageText = ""
         self.save()
