c45cb95cf91ebb46ffe1d2b8b97e7b472039765d
marios-petrov <marios.petrov23@ncf.edu>
2024-02-29 23:06:56 -0500

    Doodlespace implemented and functioning on desktop, getting it reworked so that it looks better on mobile

--- Features/views.py
+++ Features/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.utils.dateparse import parse_time
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from .models import *
 
 # Create your views here.
+def doodlespace(request):
+    return render(request, 'doodlespace.html')
