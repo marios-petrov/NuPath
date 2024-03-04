9e663c2df2eea93f411c02f13183c6f80701bcb3
goneal26 <g.oneal26@ncf.edu>
2024-03-04 11:16:33 -0500

    Tried fixing up css/bootstrap issues
    Now that we've had our meeting though where we officially finalized a bootstrap version these commits might be obsolete. I'll rework tho. Also started templating the model for a calendar event

--- Features/models.py
+++ Features/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class CalendarEvent(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
-	start_time = models.DateTimeField()
+	start_time = models.DateTimeField() # dang is this all the event needs? feels too simple
 	# end-time, whether it repeats, etc to be added later (stretch goal)
