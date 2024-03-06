9f401093d2e4d6fbc96bbd546a8d0adc9d2879c2
goneal26 <g.oneal26@ncf.edu>
2024-03-05 19:31:41 -0500

    Bones for functional calendar
    Just gotta get it to run, I'm having issues with django not actually creating the table in the database. Everything should be good though, just gotta test some migration stuff maybe?

--- Features/models.py
+++ Features/models.py
@@ -4,7 +4,18 @@ from django.db import models
 
 # model for a calendar event
 class CalendarEvent(models.Model):
+	"""
+    Represents a calendar event.
+
+    Attributes:
+        title (str): The title of the event.
+        startTime (datetime): The start time of the event.
+        description (str): The description of the event (optional).
+    """
 	title = models.CharField(max_length=100)
-	description = models.TextField()
+	description = models.TextField(blank=True, default="")
 	start_time = models.DateTimeField() # dang is this all the event needs? feels too simple
 	# end-time, whether it repeats, etc to be added later (stretch goal)
+
+	def __str__(self):
+		return 'CalendarEvent ' + self.title
--- Features/views.py
+++ Features/views.py
@@ -1,7 +1,8 @@
 from django.utils.dateparse import parse_time
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_http_methods
-from .models import *
+from .models import CalendarEvent
+from .forms import AddCalendarEvent
 
 # Create your views here.
 def doodlespace(request):
@@ -11,5 +12,11 @@ def home(request):
     return render(request, 'Features/home.html')
 
 def calendar(request):
-    # TODO (Tre)
-    return render(request, 'Features/calendar.html')
+    calendar_events = CalendarEvent.objects.all();
+    if request.method == 'POST':
+        event_form = AddCalendarEvent(request.POST or None)
+        if event_form.is_valid():
+            event_form.save()
+            return render(request, 'Features/calendar.html', {'events': calendar_events})
+    else:
+        return render(request, 'Features/calendar.html', {'events': calendar_events})
