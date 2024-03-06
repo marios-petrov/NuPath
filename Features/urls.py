7c397fae8f6ba62e320043935457fdd2d93a54e8
goneal26 <g.oneal26@ncf.edu>
2024-03-05 22:09:36 -0500

    Calendar almost done!
    Calendar very close to finsihed, just gotta get backend worked out and then make the css more responsive

--- Features/urls.py
+++ Features/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+path('delete_calendar_event/', views.delete_calendar_event, name='delete_calendar_event'),
 path('doodlespace/', views.doodlespace, name='doodlespace'),
 path('home/', views.home, name='home'),
 path('calendar/', views.calendar, name='calendar')
--- Features/views.py
+++ Features/views.py
@@ -1,6 +1,7 @@
 from django.utils.dateparse import parse_time
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
+from django.http import JsonResponse
 from .models import CalendarEvent
 from .forms import AddCalendarEvent
 
@@ -10,6 +11,22 @@ def doodlespace(request):
 
 def home(request):
     return render(request, 'Features/home.html')
+
+# view for deleting calendar event, GPT chat link that helped create it below:
+# https://chat.openai.com/share/ef154ed4-f499-4712-890c-9113af4dfe38
+@require_POST
+def delete_calendar_event(request):
+    # Extract the event ID from the request data
+    event_id = request.POST.get('event_id')
+    
+    # Retrieve the event object from the database
+    event = get_object_or_404(CalendarEvent, pk=event_id)
+    
+    # Delete the event from the database
+    event.delete()
+    
+    # Return a success response
+    return JsonResponse({'success': True})
 
 def calendar(request):
     calendar_events = CalendarEvent.objects.all();
