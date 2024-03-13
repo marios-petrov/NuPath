5650113b576bcfa1d806a96ce32b58119ebf1798
goneal26 <g.oneal26@ncf.edu>
2024-03-12 22:06:32 -0400

    Events connected to users and Notifications functioning!
    My solution for notifications is a bit of a last-minute ducttape solution but it does indeed work

--- Features/models.py
+++ Features/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -11,11 +13,14 @@ class CalendarEvent(models.Model):
         title (str): The title of the event.
         startTime (datetime): The start time of the event.
         description (str): The description of the event (optional).
+        author (User): The user associated with the calendar event
     """
 	title = models.CharField(max_length=100)
 	description = models.TextField(blank=True, default="")
 	start_time = models.DateTimeField() # dang is this all the event needs? feels too simple
+	author = models.ForeignKey(User, on_delete=models.CASCADE) # i think cascade should be okay
+	# CASCADE = delete other things with a reference to this
 	# end-time, whether it repeats, etc to be added later (stretch goal)
 
 	def __str__(self):
-		return f'Event{self.id} {self.title} @ {self.start_time}'
+		return f'Event{self.id} {self.title} @ {self.start_time} by user {self.author.username}'
--- Features/views.py
+++ Features/views.py
@@ -4,17 +4,22 @@ from django.views.decorators.http import require_http_methods, require_POST
 from django.http import JsonResponse
 from .models import CalendarEvent
 from .forms import AddCalendarEvent
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
+@login_required
 def doodlespace(request):
     return render(request, 'Features/doodlespace.html')
 
+@login_required
 def home(request):
     return render(request, 'Features/home.html')
 
 # view for deleting calendar event, GPT chat link that helped create it below:
 # https://chat.openai.com/share/ef154ed4-f499-4712-890c-9113af4dfe38
 @require_POST
+@login_required
 def delete_calendar_event(request):
     # Extract the event ID from the request data
     event_id = request.POST.get('event_id')
@@ -29,13 +34,23 @@ def delete_calendar_event(request):
     return JsonResponse({'success': True})
 
 @require_POST
+@login_required
 def add_calendar_event(request):
     # not ideal to have another view but it fixed the glaring dupe bug
     event_form = AddCalendarEvent(request.POST or None)
     if event_form.is_valid():
-        event_form.save()
+        new_event = event_form.save(commit=False)
+        new_event.author = request.user
+        new_event.save()
+        # shouldn't need a many-to-many save afaik, event doesn't explicitly have that relationship with anything
         return redirect('/calendar/')
 
+@login_required
 def calendar(request):
-    calendar_events = CalendarEvent.objects.all();
-    return render(request, 'Features/calendar.html', {'events': calendar_events})
+    calendar_events = CalendarEvent.objects.all().filter(author = request.user);
+    today = datetime.datetime.now()
+    start = today.replace(hour=0, minute=0, second=0, microsecond=0)
+    end = today.replace(hour=23, minute=59, second=59, microsecond=999999)
+    todays_events = CalendarEvent.objects.filter(author=request.user, start_time__range=(start, end))
+    # ^ the events filtered for the ones happening today
+    return render(request, 'Features/calendar.html', {'events': calendar_events, 'todays_events': todays_events})
